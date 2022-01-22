afaab078fb570639a43f19741a37073ebaf3c981
yukinissie <bigtree2490@outlook.jp>
2022-01-22 11:25:03 +0900

    init

---
+++ src/main/java/com/yukinissie/studyspringframework/StudySpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.yukinissie.studyspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yukinissie/studyspringframework/StudySpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukinissie.studyspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
